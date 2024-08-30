e56d400ce4257b0fe7f491e10082d748d6235080
ColinVanderMeer <colinvandermeer@gmail.com>
2024-08-29 21:49:48 -0400

    Add moray to outside

--- interactableMap.h
+++ interactableMap.h
@@ -23,7 +23,7 @@ struct interactableObject sign2 = {
     .x = 50,
     .y = 16,
     .sprite = &sign,
-    .numMessages = 1,
+    .numMessages = 2,
     .messages = {
         L"I'm a evil sign... I'm going to over flow the text buffer and crash the game!",
         L"Wait what do you mean it's been patched?",
@@ -41,6 +41,22 @@ struct interactableObject sign3 = {
     }
 };
 
+struct interactableObject morayObj = {
+    .x = 120,
+    .y = 60,
+    .sprite = &moray,
+    .numMessages = 7,
+    .messages = {
+        L"What do you MEAN I'm in the wrong game!?!?",
+        L"That's absurd!",
+        L"Absolutely ridiculous!",
+        L"Unbelievable!",
+        L"Outrageous!",
+        L"Preposterous!",
+        L"I'm only here because someone can't draw sprites                            {They're right...}",
+    }
+};
+
 struct interactableObject* houseObjects[] = {
     &sign1,
     &sign2,
@@ -50,6 +66,7 @@ const int numHouseObjects = sizeof(houseObjects) / sizeof(houseObjects[0]);
 
 struct interactableObject* outsideObjects[] = {
     &sign3,
+    &morayObj,
 };
 
 const int numOutsideObjects = sizeof(outsideObjects) / sizeof(outsideObjects[0]);
